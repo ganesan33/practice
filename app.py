0b9db4f97ece79930fb36a7f51fc1ae55ed18696
GANESAN G <230701089@rajalakshmi.edu.in>
2025-12-05 07:58:32 +0000

    update pipeline

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello from Flask API!"})
+    return render_template('hello.html')
 
 @app.route('/status')
 def status():
