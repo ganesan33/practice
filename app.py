db76b1d460bc3eb9ac851417e3a6ef00b9aab158
GANESAN G <230701089@rajalakshmi.edu.in>
2025-12-05 13:36:00 +0000

    upda

--- app.py
+++ app.py
@@ -1,42 +1,39 @@
-from flask import Flask, jsonify, render_template,request
+from flask import Flask, jsonify, render_template, request
 from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
 
+# MongoDB connection
 url = os.getenv("MONGO_URL")
 client = MongoClient(url)
-
-db = client.get_database('practice')
-collection = db.get_collection('devops')
+db = client["practice"]
+collection = db["devops"]
 
 @app.route('/')
 def home():
-    data = list(collection.find({}, {'_id': 0}))
+    data = list(collection.find({}, {"_id": 0}))
     return render_template('hello.html', data=data)
-
-@app.route('/status')
-def status():
-    return jsonify({"status": "API is running smoothly."})
-
-@app.route('/data/<int:id>', methods=['GET'])
-def hello(id):
-    return jsonify(f"Hello {id} welcone to this page")
 
 @app.route('/add', methods=['POST'])
 def add_data():
-    content = request.json
-    id = content["id"]
-    name = content["name"]
+    try:
+        content = request.get_json()   # IMPORTANT
+        id = content["id"]
+        name = content["name"]
 
-    data = {"id": id, "name": name}
-    collection.insert_one(data)
-    return jsonify({"message": "Data added successfully", "data": data})
+        data = {"id": id, "name": name}
+        collection.insert_one(data)
+
+        return jsonify({"message": "Data added successfully", "data": data}), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/get', methods=['GET'])
 def get_data():
-    data = list(collection.find({}, {'_id': 0}))
+    data = list(collection.find({}, {"_id": 0}))
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
