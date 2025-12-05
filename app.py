8baff0c9c4bf82737fa82ba35359004f408c81a6
GANESAN G <230701089@rajalakshmi.edu.in>
2025-12-05 12:21:50 +0000

    update mongodb

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify,render_template
+from pymongo import MongoClient
+import os
+
 
 app = Flask(__name__)
+url = os.getenv("MONGO_URL")
+client = MongoClient(url)
+db = client.get_database('practice')
 
 @app.route('/')
 def home():
@@ -13,5 +19,11 @@ def status():
 @app.route('/data/<int:id>', methods=['GET'])
 def hello(id):
     return jsonify(f"Hello {id} welcone to this page")
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
+    
+    
